e2ad7a9279d27461049e6384b4034d318000870c
francescoautera <francesco.autera@gmail.com>
2021-01-10 16:26:02 +0100

    Menu & Restart

---
+++ EscapeBalls/Assets/Scripts/MenuSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSettings : MonoBehaviour
+{
+    public void PlayGame() {
+
+        SceneManager.LoadScene("GamePlay");
+
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
