f8cdf655bded6aacd01626e17ba9f1886d6464aa
francescoautera <francesco.autera@gmail.com>
2021-01-14 11:05:17 +0100

    First commit

---
+++ BreakWall/Assets/script/BallAction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallAction : MonoBehaviour
+{
+    Rigidbody rb;
+   
+    Vector3 moveback;
+
+    
+    [SerializeField] private float velocity = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        moveback = new Vector3(0, -1,Random.Range(-1,1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += moveback * velocity * Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            moveback.y = -moveback.y;
+
+        }
+        if (collision.gameObject.tag == "Block")
+        {
+            moveback.y = -moveback.y;
+            Destroy(collision.gameObject);
+
+        }
+        if (collision.gameObject.tag == "WallSx"|| collision.gameObject.tag == "WallDx") {
+            moveback.z = -moveback.z;
+        }
+
+        if (collision.gameObject.tag == "Wall") { 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
+        }
+    }
+}
---
+++ BreakWall/Assets/script/MovePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private float vel = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0,0,Input.GetAxis("Vertical")) * Time.deltaTime*vel;
+    }
+}
